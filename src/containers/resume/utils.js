d74c42703ff3b90c55681bdb1d7283ec85fb6e92
suvidh <kaushiksuvidh6@gmail.com>
2024-07-25 11:23:28 +0530

    portfolio-1

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,30 @@
+export const data = {
+    experience: [
+      {
+        title: "Academic Volunteer(MAD)",
+        subTitle: "Guntur, India",
+        description:
+          "",
+      }
+    ],
+    education: [
+      {
+        title: "Secondary",
+        subTitle: "ABC School",
+        description:
+          "10th Grade",
+      },
+      {
+        title: "Higher Secondary",
+        subTitle: "ABC School of higher education",
+        description:
+          "12th Grade in Science",
+      },
+      {
+        title: "B.Tech",
+        subTitle: "XYZ institute of technology",
+        description:
+          "Bachelor of technology in computer science",
+      },
+    ],
+  };
